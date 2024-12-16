ea0a93df69e9293fa19a8a79f40303a718ab74f1
Hoàng Văn Nhân <hoangthienret@gmail.com>
2024-12-16 21:51:28 +0700

    Adding Employee list, Salary history interface

---
+++ src/main/java/vn/hust/bookstore/controller/SalaryHistoryController.java
@@ -0,0 +1,24 @@
+package vn.hust.bookstore.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+public class SalaryHistoryController {
+    @FXML
+    private TableView<SalaryHistory> tableSalaryHistory;
+
+    private ObservableList<SalaryHistory> salaryData = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    public void closeWindow() {
+        Stage stage = (Stage) tableSalaryHistory.getScene().getWindow();
+        stage.close();
+    }
+}
