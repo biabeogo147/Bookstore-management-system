d987cbe4ab65662222f6cd0023806a66369bd5f6
biabeogo147 <biabeogo147@gmail.com>
2024-12-16 19:40:42 +0700

    update

---
+++ src/main/java/vn/hust/bookstore/entity/SalaryHistory.java
@@ -0,0 +1,23 @@
+package vn.hust.bookstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "salary_history")
+public class SalaryHistory {
+    @Id
+    @GeneratedValue(generator = "increment")
+    private Long id;
+    private Date date;
+    private Double salary;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+}
